6005775807cb5c45d0f1c31d4c9a3799cc441e8c
ryabov-si <102360705+akkago@users.noreply.github.com>
2025-07-19 14:13:57 +0300

    fix excessive code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Container, Typography, Box } from "@mui/material";
 import { ReportSettings } from "./ReportSettings";
 import { ReportTable } from "./ReportTable";
